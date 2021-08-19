0070ca5143727ba46254d836f70b3d92d2c144d9
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-08-19 16:59:37 +0100

    begin writing first test

---
+++ spec/one_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../one'
+
+describe WordMachine do
+  wordmachine = WordMachine.new
+  url = 'https://dev-assessment.anvil.app/_/api/docs/1'
+
+  # it 'should return a list' do
+  #   expect { wordmachine.get_words(url) }.to output('SOMETHING TO THE CONSOLE').to_stderr
+  # end
+end
