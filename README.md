0300e47b65e13bd9ae5e941c7d5e9f8d33f5a557
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-08-19 16:08:56 +0100

    clean up one.rb, expand README

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
+# Word Machine
+
 Hi! Welcome to my code. I had a great time working on these challenges.
-I've written it in Ruby. I use three gems which will need local installation using:
-$ gem install GEM_NAME
+I've written it in Ruby
+
+## Installation
+
+I use three gems which will need local installation using:
+```bash
+$ gem install 'json'
+$ gem install 'rest-client'
+$ gem install 'rufus-scheduler'
+```
+
+## Usage
+
+Run the following in your command lines for each file: 
+
+one (You'll need to provide a URL): 
+
+```bash
+ruby -r "./one.rb" -e "WordMachine.new.get_words 'INSERT_URL_HERE'"
+```
+two (URLs are provided in this example):
+```bash
+ ruby -r "./two.rb" -e "FancyWordMachine.new.thread_machine 'https://dev-assessment.anvil.app/_/api/docs/1?slow=true,  https://dev-assessment.anvil.app/_/api/docs/2?slow=true, https://dev-assessment.anvil.app/_/api/docs/3?slow=true'"
+```
 The command line inputs are given at the start of each file.
 
--- one.rb
+++ one.rb
@@ -10,34 +10,26 @@ class WordMachine
     p 'Word Machine initialized'
   end
 
-  # Parse the json from the url provided
   def get_words(url)
     response = RestClient.get(url)
     result = JSON.parse(response)
-    # Add the word to the array of words
     @all_words << result['text']
-    # If there a 'next page' in the JSON then repeat previous steps
     if result.key?('next_page')
       get_words(result['next_page'])
     else
-      # If there isn't a 'next page' then count the words
       count_words(@all_words)
     end
   end
 
-  # Count the words
   def count_words(paragraph)
     words = paragraph.join.split(' ')
     frequencies = Hash.new(0)
-    # Strip the words of non alpha-numerics and add their frequency to the hash
     words.each { |word| frequencies[word.downcase.gsub(/\W+/, '')] += 1 }
     sort_words(frequencies)
   end
 
-  # Then sort the words
   def sort_words(frequencies)
     sorted = frequencies.sort { |(k1, v1), (k2, v2)| v1 == v2 ? k1 <=> k2 : v2 <=> v1 }.to_h
-    # sorted.each { |k, v| print `#{k}:#{v}` }
     sorted.each do |key, value|
       puts "#{key}:#{value}"
     end
